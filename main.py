6133099eac0dc07a24fb49f105a78cdbe1216cbe
alexmal01 <01161504@pw.edu.pl>
2021-04-20 23:40:11 +0200

    Zadanie 1.2

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from fastapi import FastAPI
 app = FastAPI()
 @app.get("/")
-def root():
+def root_view():
     return {"message" : "Hello world!"}
+
+@app.get("/method/{name}")
+async def method_name_view(name: str):
+    return {"method" : name}
