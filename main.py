6ca8905cb59d98cb6f3f02f1b70be8f83229b11d
alexmal01 <01161504@pw.edu.pl>
2021-04-21 00:43:35 +0200

    Typowanie

--- main.py
+++ main.py
@@ -1,9 +1,28 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
 app = FastAPI()
+app.counter = 0
+
+
+class HelloResp(BaseModel):
+    msg: dict
+
+
 @app.get("/")
-def root_view():
-    return {"message" : "Hello world!"}
+def root():
+    return {"message": "Hello world!"}
+
+
+@app.get("/counter")
+def counter():
+    app.counter += 1
+    return app.counter
 
 @app.get("/{name}")
-async def method_name_view(name: str):
-    return {"method" : f"{name}"}
+def method_view(name:str):
+    return {"method":name}
+
+@app.get("/hello/{name}", response_model=HelloResp)
+def hello_name_view(name: str):
+    return HelloResp(msg={"method": name})
